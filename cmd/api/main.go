93e5fe6ed43349d3c96e509aef30eb03d3de4c1a
Miguel Avila <98852302+MiguelAAvila@users.noreply.github.com>
2022-10-01 22:53:14 -0600

    added code for healthcheck - server health data

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,58 @@
-// File: cmd/api/main.go
- package main 
-import "fmt" 
-func main() { 
-fmt.Println("Starting Server....")
+//Filename: cmd/api/main.go
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// App Version
+const version = "1.0.0"
+
+// App config
+type config struct {
+	port int
+	env  string // dev, stg, prd, etc...
+}
+
+// dependencies injections
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+	//read in the flag that are needed to populate the config ~ flag for using as extra cmd
+	flag.IntVar(&cfg.port, "port", 4000, "API port")
+	flag.StringVar(&cfg.env, "env", "dev", "(dev | stg | prd")
+	flag.Parse()
+
+	//create a logger ~ use := for undeclared var
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	//create install of out app
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	//create our http server
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("Starting %s server at %s", cfg.env, srv.Addr)
+	//start the server
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+
 }
