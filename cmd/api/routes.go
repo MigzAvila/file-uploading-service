8792107a54b6687e2987f7a1ff2259c26d60c40d
Miguel Avila <98852302+MiguelAAvila@users.noreply.github.com>
2022-10-01 23:11:40 -0600

    added code for centralized error handling

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,6 +11,8 @@ import (
 func (app *application) routes() *httprouter.Router {
 
 	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.MethodNotAllowedReponse)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/randomized/:id", app.randomizedHandler)
 
